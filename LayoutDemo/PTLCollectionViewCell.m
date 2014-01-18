5ad82bc0e275085c5d9dab01b570708f2cdb6e16
Brian Partridge <brianpartridge@gmail.com>
2014-01-18 17:05:07 -0500

    add a border to cells

--- LayoutDemo/PTLCollectionViewCell.m
+++ LayoutDemo/PTLCollectionViewCell.m
@@ -10,24 +10,26 @@
 
 @implementation PTLCollectionViewCell
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
-        self.layer.borderColor = [UIColor blackColor].CGColor;
-        self.layer.borderWidth = 1;
+        [self commonInit];
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+- (id)initWithCoder:(NSCoder *)aDecoder {
+	self = [super initWithCoder:aDecoder];
+	if (self) {
+	    [self commonInit];
+	}
+
+	return self;
 }
-*/
+
+- (void)commonInit {
+    self.layer.borderColor = [UIColor blackColor].CGColor;
+    self.layer.borderWidth = 1;
+}
 
 @end
