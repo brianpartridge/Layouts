30f5fb2d00f6e4fb74c63a8387aecdae84df44e9
Brian Partridge <brianpartridge@gmail.com>
2014-01-18 23:03:48 -0500

    use PTLDatasource to simplify insertions and deletions

--- LayoutDemo/PTLCollectionViewController.m
+++ LayoutDemo/PTLCollectionViewController.m
@@ -8,19 +8,23 @@
 
 #import "PTLCollectionViewController.h"
 #import "PTLCollectionViewCell.h"
+#import "UIKit+PTLDatasource.h"
 
 static NSString * const kCellId = @"Cell";
 
-@interface PTLCollectionViewController ()
+@interface PTLCollectionViewController () <PTLDatasourceObserver>
 
-@property (nonatomic, assign) NSInteger count;
+@property (nonatomic, strong) UIStepper *stepper;
+
+@property (nonatomic, strong) PTLIndexDatasource *datasource;
+@property (nonatomic, assign) NSUInteger datasourceUpdateCount;
+@property (nonatomic, strong) NSMutableArray *datasourceChanges;
 
 @end
 
 @implementation PTLCollectionViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -28,47 +32,131 @@ static NSString * const kCellId = @"Cell";
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
 
     self.title = @"Layout";
-    self.count = 10;
+
     self.collectionView.backgroundColor = [UIColor colorWithRed:0.200 green:0.558 blue:0.686 alpha:1.000];
 
     [self.collectionView registerNib:[UINib nibWithNibName:@"PTLCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:kCellId];
 
+    self.datasource = [[PTLIndexDatasource alloc] initWithIndecies:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0, 10)]];
+    self.datasource.collectionViewCellIdentifier = kCellId;
+    self.datasource.collectionViewCellConfigBlock = ^(UICollectionView *collectionView, UICollectionViewCell *cell, id item, NSIndexPath *indexPath) {
+        PTLCollectionViewCell *aCell = (PTLCollectionViewCell *)cell;
+        aCell.titleLabel.text = [NSString stringWithFormat:@"%@", item];
+    };
+    self.collectionView.dataSource = self.datasource;
+    [self.datasource addChangeObserver:self];
+
     UIStepper *stepper = [[UIStepper alloc] init];
-    stepper.value = self.count;
+    stepper.value = self.datasource.numberOfItems;
+    stepper.maximumValue = NSIntegerMax;
     [stepper addTarget:self action:@selector(countChanged:) forControlEvents:UIControlEventValueChanged];
+    self.stepper = stepper;
     UIBarButtonItem *counter = [[UIBarButtonItem alloc] initWithCustomView:stepper];
     self.navigationItem.rightBarButtonItem = counter;
+}
+
+#pragma mark - Data Helpers
+
+- (NSUInteger)lastIndexValue {
+    id item = [self.datasource.allItems lastObject];
+    return (item) ? [item unsignedIntegerValue] : NSNotFound;
+}
+
+- (NSUInteger)nextIndexValue {
+    NSUInteger index = [self lastIndexValue];
+    return (index == NSNotFound) ? 0 : ++index;
 }
 
 #pragma mark - User Interaction
 
 - (void)countChanged:(UIStepper *)stepper {
     NSInteger newValue = stepper.value;
-    if (newValue < self.count) {
-        self.count = newValue;
-        [self.collectionView deleteItemsAtIndexPaths:@[[NSIndexPath indexPathForItem:newValue inSection:0]]];
+    if (newValue < self.datasource.numberOfItems) {
+        [self.datasource removeIndexValue:[self lastIndexValue]];
     } else {
-        self.count = newValue;
-        [self.collectionView insertItemsAtIndexPaths:@[[NSIndexPath indexPathForItem:self.count-1 inSection:0]]];
+        [self.datasource addIndexValue:[self nextIndexValue]];
+    }
+    stepper.value = self.datasource.numberOfItems;
+}
+
+#pragma mark - UICollectionViewDelegate
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    id item = [self.datasource itemAtIndexPath:indexPath];
+    [self.datasource removeIndexValue:[item unsignedIntegerValue]];
+
+    // Update the stepper value accordingly
+    self.stepper.value = self.datasource.numberOfItems;
+}
+
+#pragma mark - PTLDatasourceObserver
+
+- (void)datasourceWillChange:(id<PTLDatasource>)datasource {
+    self.datasourceUpdateCount++;
+    if (self.datasourceUpdateCount == 1) {
+        self.datasourceChanges = [NSMutableArray array];
     }
 }
 
-#pragma mark - UICollectionViewDataSource
+- (void)datasourceDidChange:(id<PTLDatasource>)datasource {
+    self.datasourceUpdateCount--;
+    if (self.datasourceUpdateCount != 0) {
+        return;
+    }
 
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.count;
+    [self.collectionView performBatchUpdates:^{
+        for (NSDictionary *dict in self.datasourceChanges) {
+            PTLChangeType change = [dict[@"change"] integerValue];
+            if ([dict[@"isSectionChange"] boolValue]) {
+                NSUInteger sectionIndex = [dict[@"sectionIndex"] integerValue];
+                switch(change) {
+                    case PTLChangeTypeInsert:
+                        [self.collectionView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex]];
+                        break;
+                    case PTLChangeTypeRemove:
+                        [self.collectionView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex]];
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                NSIndexPath *indexPath = dict[@"indexPath"];
+                NSIndexPath *newIndexPath = dict[@"newIndexPath"];
+                switch(change) {
+                    case PTLChangeTypeInsert:
+                        [self.collectionView insertItemsAtIndexPaths:@[newIndexPath]];
+                        break;
+                    case PTLChangeTypeRemove:
+                        [self.collectionView deleteItemsAtIndexPaths:@[indexPath]];
+                        break;
+                    case PTLChangeTypeUpdate:
+                        [self.collectionView reloadItemsAtIndexPaths:@[indexPath]];
+                        break;
+                    case PTLChangeTypeMove:
+                        [self.collectionView moveItemAtIndexPath:indexPath toIndexPath:newIndexPath];
+                        break;
+                }
+            }
+        }
+        self.datasourceChanges = nil;
+    } completion:nil];
 }
 
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    PTLCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kCellId forIndexPath:indexPath];
-    cell.titleLabel.text = [NSString stringWithFormat:@"%d", indexPath.item];
-    return cell;
+- (void)datasource:(id<PTLDatasource>)datasource didChange:(PTLChangeType)change atIndexPath:(NSIndexPath *)indexPath newIndexPath:(NSIndexPath *)newIndexPath {
+    [self.datasourceChanges addObject:@{ @"change" : @(change),
+                                         @"indexPath" : indexPath ?: [NSNull null],
+                                         @"newIndexPath" : newIndexPath ?: [NSNull null] }];
+}
+
+- (void)datasource:(id<PTLDatasource>)datasource didChange:(PTLChangeType)change atSectionIndex:(NSInteger)sectionIndex {
+    [self.datasourceChanges addObject:@{ @"change" : @(change),
+                                         @"isSectionChange" : @YES,
+                                         @"sectionIndex" : @(sectionIndex) }];
 }
 
 @end
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, "7.0"
+
+target "LayoutDemo" do
+
+pod 'PTLDatasource', '~> 0.0.4'
+
+end
